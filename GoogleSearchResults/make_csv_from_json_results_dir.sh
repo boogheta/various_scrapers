0b91052c62101469112895379d66bada16ffcdcd
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2013-08-01 17:34:50 +0200

    add generate list uniq urls / safer shell arguments

--- GoogleSearchResults/make_csv_from_json_results_dir.sh
+++ GoogleSearchResults/make_csv_from_json_results_dir.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-dir=$1
-rm -f $dir/1-results-$dir.csv
-echo "query,url,domain,is_pdf" > $dir/1-results-$dir.csv
+dir=$(echo "$1" | sed 's#/$##')
+rm -f "$dir/1-results.csv"
+echo "query,url,domain,is_pdf" > "$dir/1-results.csv"
 
-for file in `ls $dir/json`; do
+for file in `ls "$dir/json"`; do
   query=""
-  cat $dir/json/$file | while read line; do
+  cat "$dir/json/$file" | while read line; do
     if [ -z "$query" ]; then
-      query=$(echo $line | sed 's/^{"query":"//' | sed 's/","lang".*$//' | sed 's/"/""/g')
+      query=$(echo "$line" | sed 's/^{"query":"//' | sed 's/","lang".*$//' | sed 's/"/""/g')
     else
-      url=$(echo $line | sed 's/^.*"url":"//' | sed 's/","summary".*$//' | sed 's/"/""/g')
-      ext=$(echo $url | sed 's/^.*\(....\)/\1/')
+      url=$(echo "$line" | sed 's/^.*"url":"//' | sed 's/","summary".*$//' | sed 's/"/""/g' | sed 's#^/interstitial?url=##')
+      ext=$(echo "$url" | sed 's/^.*\(....\)/\1/')
       pdf=0
       if [ "$ext" == ".pdf" ]; then
         pdf=1
       fi
-      domain=$(echo $url | sed 's#^\(https\?://[^/]*/\).*$#\1#')
-      echo '"'$query'","'$url'","'$domain'",'$pdf >> $dir/1-results-$dir.csv
+      domain=$(echo "$url" | sed 's#^\(https\?://[^/]*/\).*$#\1#')
+      echo '"'$query'","'$url'","'$domain'",'$pdf >> "$dir/1-results.csv"
     fi
   done  
 done
+
+cat "$dir/1-results.csv" | sed 's/","http[^"]*",[01]*$//' | sed 's/^.*","//' | grep -v 'query,url' | sort -u > "$dir/2-results-urls.csv"
 
