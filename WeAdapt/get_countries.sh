e8924ba8f0bbd39b69f5c07210ee902e238bbebd
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-01-21 13:12:15 +0100

    fix missing country info for france on nominatim

--- WeAdapt/get_countries.sh
+++ WeAdapt/get_countries.sh
@@ -9,6 +9,9 @@ echo "select id, latitude, longitude from placemark" |
         id=`echo "$line" | awk -F ',' '{print $1}'`;
         coords=`echo "$line" | sed 's/^'"$id"',//' | sed 's/,/\&lon=/' | sed 's/\([0-9].[0-9][0-9]\)[0-9]*/\1/g'`;
         country=`curl "http://nominatim.openstreetmap.org/reverse?format=xml&accept-language=en&email=benjamin.oogheATsciences-po.fr&lat=$coords" -s | grep country | sed 's/^.*<country>\(.*\)<\/country>.*$/\1/'`;
+        if echo $country | grep "<country_code>" > /dev/null; then
+            country="France"
+        fi
         echo "$id,$country";
     done >> placemarks_coords.csv
 
