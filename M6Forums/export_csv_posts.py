0b4026395a5e52569881412b30d0cb4a37f6b292
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-02-06 12:54:10 +0100

    fix encoding

--- M6Forums/export_csv_posts.py
+++ M6Forums/export_csv_posts.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json, sys
+import json, sys, re
 from datetime import datetime
 from pymongo import MongoClient
 
 db = MongoClient()["m6forums"]['posts']
 
-format = lambda x: '"' + x.replace('"', '""') + '"'
+re_cp1212 = re.compile(ur'[\x80-\x9F]')
+def recode(x):
+    if type(x) == unicode and re_cp1212.search(x):
+        x = x.encode('latin1').decode('CP1252', 'ignore')
+    return x.encode('utf8')
+
+re_newline = re.compile(r"[\r\n]+")
+uniline = lambda x: re_newline.sub(" ", x)
+escaper = lambda x: '"%s"' % x.replace('"', '""')
+formatt = lambda x: escaper(uniline(recode(x)))
 
 db.ensure_index([("thread_title", 1), ("created_at", 1)])
 
 print "permalink,thread,author,text,timestamp,deleted"
 for t in db.find(sort=[("thread_title", 1), ("created_at", 1)]):
-    text = format(t["message"])
-    title = format(t["thread_title"])
-    print ",".join([a.encode("utf-8") if a else "" for a in [t["permalink"],title,t["author"],text,t["created_at"],str(t["deleted"])]])
+    print ",".join([formatt(a) if a else "" for a in [t["permalink"],t["thread_title"],t["author"],t["message"],t["created_at"],str(t["deleted"])]])
 
