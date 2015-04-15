530a5dacd418446e21b4d370453b52bd55144ad2
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-04-15 12:21:05 +0200

    videogames OST downloader

---
+++ OST/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+url=$1
+if test -z "$url"; then
+  url="http://downloads.khinsider.com/game-soundtracks/album/little-big-adventure-2-twinsen-s-odyssey-original-soundtrack"
+fi
+
+dir=$(echo $url | sed 's|^.*/\([^/]\+\)/\?$|musique/\1|')
+mkdir -p "$dir"
+
+curl -sL "$url"      |
+ grep "href=.*\.mp3" |
+ sed 's/^.*href="//' |
+ sed 's/".*$//'      |
+ while read url; do
+  fileurl=$(curl -sL "$url" |
+   grep "href=.*\.mp3"      |
+   sed 's/^.*href="//'      |
+   sed 's/".*$//')
+  filename=$(echo $fileurl  |
+   sed 's|^.*/\([^/]\+\)$|\1|')
+  echo $fileurl $filename
+  wget "$fileurl" -O "$dir/$filename"
+done
