1f85a5cad0e8d47956169dc6608ca7b1ab2774ba
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-04-20 17:41:11 +0200

    add source for 2021

--- concours-ESR-PU-MC/download_pdfs.sh
+++ concours-ESR-PU-MC/download_pdfs.sh
@@ -2,22 +2,24 @@
 
 mkdir -p pdfs
 
-curl -sL https://www.galaxie.enseignementsup-recherche.gouv.fr/ensup/cand_resultats_qualification.htm |
- grep "\.pdf.*qualifié"  |
- while read line; do
-  pdfurl="https://www.galaxie.enseignementsup-recherche.gouv.fr/ensup/"$(echo $line | sed 's/<span[^>]*>//' | sed 's/ class="[^"]*"//g' | awk -F '"' '{print $2}')
-  pdftitle=$(echo $line | sed 's/^.*Liste des qualifiés //' | sed 's/<.*$//')
-  annee=$(echo $pdftitle | awk '{print $1}')
-  typ="PU"
-  extra=""
-  if echo $pdftitle | grep "conférences" > /dev/null; then
+for url in https://www.galaxie.enseignementsup-recherche.gouv.fr/ensup/CNU_qualification.htm https://www.galaxie.enseignementsup-recherche.gouv.fr/ensup/cand_resultats_qualification.htm; do
+ curl -sL "$url" |
+  grep "\.pdf.*qualifié"  |
+  while read line; do
+   pdfurl="https://www.galaxie.enseignementsup-recherche.gouv.fr/ensup/"$(echo $line | sed -r 's/^.*(<a .*Liste des qualifiés[^<]*<\/a>).*$/\1/' | awk -F '"' '{print $2}')
+   pdftitle=$(echo $line | sed 's/^.*Liste des qualifiés //' | sed 's/<.*$//')
+   annee=$(echo $pdftitle | awk '{print $1}')
+   typ="PU"
+   extra=""
+   if echo $pdftitle | grep "conférences" > /dev/null; then
     typ="MC"
-  fi
-  if echo $pdftitle | grep "Muséum" > /dev/null; then
+   fi
+   if echo $pdftitle | grep "Muséum" > /dev/null; then
     extra="-MNHN"
-  fi
-  pdffile="${annee}-qualifies-${typ}${extra}.pdf"
-  echo "Downloading $pdffile at $pdfurl ..."
-  wget --quiet "$pdfurl" -O pdfs/"$pdffile"
- done
+   fi
+   pdffile="${annee}-qualifies-${typ}${extra}.pdf"
+   echo "Downloading $pdffile at $pdfurl ..."
+   wget --quiet "$pdfurl" -O pdfs/"$pdffile"
+  done
+done
 
